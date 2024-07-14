120655e0daadcb5534d4adc5ebbe0ad5140448e6
Matt Tew <matt.tew@intellis.team>
2024-07-14 08:38:24 +0000

    Getting token

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use settings::Settings;
+mod settings;
+
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error> {
-	let settings = match settings::loadSettings()?;
+fn main() -> Result<(), Box<dyn Error>> {
+	let settings = settings::load_settings()?;
 
 	println!("Token: {}", settings.token);
 	println!("Token: {}", settings.token);
+
+	Ok(())
 }
 
--- src/settings.rs
+++ src/settings.rs
@@ -7,29 +7,32 @@ pub struct Settings {
 	pub api_url: String,
 }
 
-pub fn loadSettings() -> Result<Settings, Box<dyn Error>> {
-	let project_dirs = ProjectDirs::from("com", "Matt Tew", "OneThing");
-	let config_dir =  project_dirs.config_dir();
-	let config_file = config_dir.join("onething.toml");
+pub fn load_settings() -> Result<Settings, Box<dyn Error>> {
 
 	let config_file = match ProjectDirs::from("com", "Matt Tew", "OneThing") {
-		Some(project_dirs) => project_dirs.config_dir().join("onething.toml"),
+		Some(project_dirs) => Some(project_dirs.config_dir().join("onething.toml")),
 		None => None,
 	};
 
-	let config_builder = Config::builder();
+	let mut config_builder = Config::builder();
 
-	if Some(config_file) {
-		config_builder.add_source(config::File::with_name(config_file.to_str()?));
+	if config_file.is_some() {
+		config_builder = config_builder.add_source(config::File::with_name(config_file.unwrap().to_str().unwrap()));
 	}
 
-	let config_values = match config_builder
+	let config_values = config_builder
 		.add_source(config::Environment::with_prefix("ONETHING"))
 		.build();
 
 	match config_values {
-		Ok(config) => {
-			config.try_into()
+		Ok(config_values) => {
+			let token = config_values.get_string("token")?;
+			let api_url = match config_values.get_string("api_url") {
+				Ok(url) => url,
+				Err(_) => "https://api.matt.tew.io/".to_string(),
+			};
+
+			Ok(Settings { token, api_url })
 		}
 		Err(e) => Err(Box::new(e)),
 	}
